01eedbc3e2c916906f83ffb69c82ef055b05c8fe
Seth Bruzewski <bruzewskis@gmail.com>
2022-10-31 14:31:35 -0600

    fixed path

--- bisbas.py
+++ bisbas.py
@@ -136,7 +136,7 @@ def main(args):
     #write_stack = readers.DataStack.empty_like(read_stack, outdir, dates)
 
     with bf.get_default_pipeline() as PIPELINE1:
-        b_read = bisblocks.IntfReadBlock([files], 1000, 1, 'f32', files)
+        b_read = bisblocks.IntfReadBlock([intfs_dir], 1000, 1, 'f32', files)
         b_print = bisblocks.PrintStuffBlock(b_read)
 
         PIPELINE1.run()
