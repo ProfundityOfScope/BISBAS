ec06636adbeb54d8bc1d630d9d59acc05bb894c0
Seth Bruzewski <bruzewskis@gmail.com>
2022-11-07 15:38:37 -0700

    remove context

--- bisbas.py
+++ bisbas.py
@@ -135,11 +135,12 @@ def main(args):
     #os.makedirs(outdir, exist_ok=True)
     #write_stack = readers.DataStack.empty_like(read_stack, outdir, dates)
 
-    with bf.get_default_pipeline() as PIPELINE1:
-        b_read = bisblocks.IntfReadBlock([path], 9750, 'f32', files)
-        b_print = bisblocks.PrintStuffBlock(b_read)
+    #with bf.get_default_pipeline() as PIPELINE1:
+    b_read = bisblocks.IntfReadBlock([path], 9750, 'f32', files)
+    b_print = bisblocks.PrintStuffBlock(b_read)
 
-        PIPELINE1.run()
+    PIPELINE1 = bf.get_default_pipeline()
+    PIPELINE1.run()
     #picks = np.arange(0, read_stack.imsize).reshape(-1, gulp)
     #picks = picks[picks.shape[0]//2-50 : picks.shape[0]//2+50]
     #p = mp.Pool(mp.cpu_count()-1)
