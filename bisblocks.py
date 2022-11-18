a84152a64040156c306672fe59e01cea0f9e4e19
Seth Bruzewski <bruzewskis@gmail.com>
2022-11-18 13:34:38 -0700

    rapidfire

--- bisblocks.py
+++ bisblocks.py
@@ -165,8 +165,8 @@ class GenTimeseriesBlock(bfp.TransformBlock):
         # Mask out low-rank values
         lowrank = np.linalg.matrix_rank(A) != self.nd - 1
         print(lowrank[0])
-        A[lowrank] = np.eye(self.nd-1)
-        B[lowrank] = np.full(self.nd-1, np.nan)
+        A[lowrank,:,:] = np.eye(self.nd-1)
+        B[lowrank,:] = np.full(self.nd-1, np.nan)
         print(A[0], B[0])
 
         # Solve
