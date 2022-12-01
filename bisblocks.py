65aea38e9d6335dc69a19f50c2d3922ec7cc4189
Seth Bruzewski <bruzewskis@gmail.com>
2022-12-01 15:13:24 -0700

    tuple is fine

--- bisblocks.py
+++ bisblocks.py
@@ -110,7 +110,7 @@ class IntfReadBlock(bfp.SourceBlock):
                 'ycoords':  ireader.ycoords.astype(np.float64).tobytes(),
                 'xname':    ireader.xname,
                 'yname':    ireader.yname,
-                'imshape':  ireader.imshape.tolist(),
+                'imshape':  ireader.imshape,
                 '_tensor':  {'dtype':  self.dtype,
                              'shape':  [-1, self.gulp_pixels, len(self.file_order)],
                             },
