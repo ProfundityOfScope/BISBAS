a7c3549ebfdff41cdbcb9dd7472a8623a2975913
Seth Bruzewski <bruzewskis@gmail.com>
2023-02-20 12:23:59 -0700

    testing

--- readers.py
+++ readers.py
@@ -353,6 +353,7 @@ class DataStack():
             subdata = self.data_near(x0, y0, chunk_size)
 
             # Check to see if we have enough good pixels
+            print('='*20, subdata.shape)
             num_good_per_date = np.sum(~np.isnan(subdata), axis=1)
             mngpd = np.min(num_good_per_date)
             readerslogger.debug(f'chunk_size={chunk_size} worst intf has {mngpd} valid pixels')
