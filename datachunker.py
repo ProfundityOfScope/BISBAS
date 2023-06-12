c94ff897c0755253273ac2966a37d1d8f6979ff2
Seth Bruzewski <bruzewskis@gmail.com>
2023-06-11 22:38:50 -0600

    chunk

--- datachunker.py
+++ datachunker.py
@@ -7,11 +7,12 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
 	t0,y0,x0 = fo['coherence'].shape
 	attrs = dict(fo.attrs)
 
-	for n in range(1,5):
+	for n in range(1,7):
+		for m in range(1,7):
 
-		tn = int(t0/2**n)
-		yn = int(y0/2**n)
-		xn = int(x0/2**n)
-		name = f'ifgs_{n:02d}.h5'
+			tn = int(t0/2**n)
+			yn = int(y0/2**m)
+			xn = int(x0/2**m)
+			name = f'ifgs_t{2**n:02d}_p{2**m:02d}.h5'
 
-		print(tn, yn, xn, name)
+			print(tn, yn, xn, name)
