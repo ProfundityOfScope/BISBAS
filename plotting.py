274c9574fe21b69742a16c1d19e8f72ec95cbea4
Seth Bruzewski <bruzewskis@gmail.com>
2023-06-14 15:54:29 -0600

    scaling

--- plotting.py
+++ plotting.py
@@ -30,10 +30,11 @@ plotslogger = logging.getLogger('__main__')
 def make_video(data, dates, outfile, fps=10):
 
     # Set up the figure and axis
-    fig, ax = plt.subplots(figsize=(5, 5), dpi=1080/5)
+    aspect = data.shape[2]/data.shape[1]
+    fig, ax = plt.subplots(figsize=(5, 5*aspect), dpi=1080/5)
 
-    imkwargs = {'vmin': 0, 'vmax': 6000, 'interpolation': 'nearest',
-                'origin': 'lower', 'rasterized': True}
+    imkwargs = {'vmin': -15, 'vmax': 15, 'interpolation': 'nearest',
+                'origin': 'lower', 'rasterized': True, 'cmap': 'Spectral_r'}
 
     def update(frame):
         ax.clear()
@@ -43,20 +44,21 @@ def make_video(data, dates, outfile, fps=10):
         return (im,)
 
     ani = FuncAnimation(fig, update, frames=len(data), blit=True)
-    ani.save(outfile, writer='ffmpeg', fps=fps)
+    ani.save(outfile, writer='ffmpeg', fps=fps, bbox_inches='tight')
     plt.close(fig)
 
 
 def interp_video(data, dates, outfile, fps, nframes=None):
 
     # Set up the figure and axis
-    fig, ax = plt.subplots(figsize=(5, 5), dpi=1080/5)
+    aspect = data.shape[2]/data.shape[1]
+    fig, ax = plt.subplots(figsize=(5, 5*aspect), dpi=1080/5)
 
     nframes = len(data) if nframes is None else nframes
     tinterp = np.linspace(dates.min(), dates.max(), nframes, endpoint=False)
 
-    imkwargs = {'vmin': 0, 'vmax': 6000, 'interpolation': 'nearest',
-                'origin': 'lower', 'rasterized': True}
+    imkwargs = {'vmin': -15, 'vmax': 15, 'interpolation': 'nearest',
+                'origin': 'lower', 'rasterized': True, 'cmap': 'Spectral_r'}
 
     def update(frame):
         ax.clear()
@@ -74,5 +76,5 @@ def interp_video(data, dates, outfile, fps, nframes=None):
         return (im,)
 
     ani = FuncAnimation(fig, update, frames=nframes, blit=True)
-    ani.save(outfile, writer='ffmpeg', fps=fps)
+    ani.save(outfile, writer='ffmpeg', fps=fps, bbox_inches='tight')
     plt.close(fig)
