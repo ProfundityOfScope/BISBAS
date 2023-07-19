e7da74b5c63edaa17521c6953cf605f9270983b0
Seth <bruzewskis@gmail.com>
2023-07-19 16:45:20 -0600

    sdfgas

--- paritytesting.py
+++ paritytesting.py
@@ -34,7 +34,8 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
         
         # pure numpy
         M = np.diag(~np.isnan(phases))
-        A = np.linalg.multi_dot([G.T, M, G])/10
+        A = np.linalg.multi_dot([G.T, M, G])
+        A /= np.max(A)
         det = np.linalg.det(A)
         sign, logdet = np.linalg.slogdet(A)
         rank = np.linalg.matrix_rank(A, hermitian=True)
@@ -43,7 +44,8 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
         # pure cupy 64
         Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
         Gc = cp.asarray(G)
-        Ac = cp.dot(Gc.T, Mc).dot(Gc)/10
+        Ac = cp.dot(Gc.T, Mc).dot(Gc)
+        A /= cp.max(Ac)
         det = cp.linalg.det(Ac)
         sign, logdet = cp.linalg.slogdet(Ac)
         rank = cp.linalg.matrix_rank(Ac)
@@ -54,6 +56,7 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
             Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
             Gc = cp.asarray(G)
             Ac = cp.dot(Gc.T, Mc).dot(Gc)/10
+            Ac /= cp.max(Ac)
             det = cp.linalg.det(Ac)
             sign, logdet = cp.linalg.slogdet(Ac)
             rank = cp.linalg.matrix_rank(Ac)
