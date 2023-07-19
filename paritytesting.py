490dc0ac15ce40218da5d61ac873d1b878447a0f
Seth <bruzewskis@gmail.com>
2023-07-19 16:10:06 -0600

    sdfgas

--- paritytesting.py
+++ paritytesting.py
@@ -31,23 +31,34 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
         
         print(f'Coordinate: ({ci}, {cj}) with notes: "{notes[i]}"')
         mask = (cohers<0.4).astype(int)
-        print(mask)
         
         # pure numpy
         M = np.diag(~np.isnan(phases))
         A = np.linalg.multi_dot([G.T, M, G])
+        B = np.dot(G.T, M) * phases
+        
         det = np.linalg.det(A)
         sign, logdet = np.linalg.slogdet(A)
         rank = np.linalg.matrix_rank(A, hermitian=True)
+        try:
+            model = np.linalg.solve(A, B)
+        except Exception as e:
+            print(e)
         print('\tPure numpy(64?):', det, sign, logdet, rank)
     
         # pure cupy 64
         Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
         Gc = cp.asarray(G)
         Ac = cp.dot(Gc.T, Mc).dot(Gc)
+        Bc = cp.dot(Gc.T, Mc) * cp.asarray(phases)
+        
         det = cp.linalg.det(Ac)
         sign, logdet = cp.linalg.slogdet(Ac)
         rank = cp.linalg.matrix_rank(Ac)
+        try:
+            modelc = cp.linalg.solve(Ac, Bc)
+        except Exception as e:
+            print(e)
         print('\tPure Cupy(64?):', det, sign, logdet, rank)
         
         # cupy with errstate 64
@@ -55,8 +66,14 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
             Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
             Gc = cp.asarray(G)
             Ac = cp.dot(Gc.T, Mc).dot(Gc)
+            Bc = cp.dot(Gc.T, Mc) * cp.asarray(phases)
+            
             det = cp.linalg.det(Ac)
             sign, logdet = cp.linalg.slogdet(Ac)
             rank = cp.linalg.matrix_rank(Ac)
+            try:
+                modelc = cp.linalg.solve(Ac, Bc)
+            except Exception as e:
+                print(e)
             print('\tErrstate Cupy(64):', det, sign, logdet, rank)
             
