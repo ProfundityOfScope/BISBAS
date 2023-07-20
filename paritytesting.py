2210ae7e4f798fd97048ea50fe9497c4d00307d8
Seth <bruzewskis@gmail.com>
2023-07-20 13:27:46 -0600

    sdfgas

--- paritytesting.py
+++ paritytesting.py
@@ -9,7 +9,9 @@ Created on Wed Jul 19 14:51:05 2023
 import cupy as cp
 import cupyx as cpx
 import numpy as np
+import scipy as sp
 import h5py
+from time import time
 
 coi = [ (2291, 2270), (2291, 2271), (2291, 2273), (2285, 2274)]
 notes = ['normal', 'det0, tsbig', 'det0, tsnormal', 'det0, tsflagged']
@@ -22,7 +24,7 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
     nd = np.size(G, 1)
     out = np.zeros((len(coi), nd, nd))
     for i in range(len(coi)):
-        ci,cj = coi[i]
+        ci, cj = coi[i]
         
         phases = fo['unwrapPhase'][:,ci,cj]
         cohers = fo['coherence'][:,ci,cj]
@@ -39,31 +41,28 @@ with h5py.File('ifgramStack.h5', 'r') as fo:
         A = np.linalg.multi_dot([G.T, M, G])
         out[i] = A
         
+        start = time()
+        dettest = np.isinf(np.linalg.slogdet(A)[1])
+        dettime = time() - start
+        start = time()
+        lu,piv = sp.linalg.lu_factor(A)
+        lutest = np.any(np.diag(np.triu(lu))<1e-10)
+        lutime = time() - start
         
-        # det = np.linalg.det(A)
-        # sign, logdet = np.linalg.slogdet(A)
-        # rank = np.linalg.matrix_rank(A, hermitian=True)
-        # print('\tPure numpy(64?):', det, sign, logdet, rank)
+        print('\tPure numpy(64?):', dettest, dettime, lutest, lutime)
     
-        # # pure cupy 64
-        # Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
-        # Gc = cp.asarray(G)
-        # Ac = cp.dot(Gc.T, Mc).dot(Gc)
+        # pure cupy 64
+        Mc = cp.asarray(M)
+        Gc = cp.asarray(G)
+        Ac = cp.asarray(A)
         
-        # det = cp.linalg.det(Ac)
-        # sign, logdet = cp.linalg.slogdet(Ac)
-        # rank = cp.linalg.matrix_rank(Ac)
-        # print('\tPure Cupy(64?):', det, sign, logdet, rank)
-        
-        # # cupy with errstate 64
-        # with cpx.errstate(linalg='raise'):
-        #     Mc = cp.diag(~cp.isnan(cp.asarray(phases)))
-        #     Gc = cp.asarray(G)
-        #     Ac = cp.dot(Gc.T, Mc).dot(Gc)
-            
-        #     det = cp.linalg.det(Ac)
-        #     sign, logdet = cp.linalg.slogdet(Ac)
-        #     rank = cp.linalg.matrix_rank(Ac)
-        #     print('\tErrstate Cupy(64):', det, sign, logdet, rank)
+        start = time()
+        dettestc = cp.isinf(cp.linalg.slogdet(Ac)[1])
+        dettimec = time() - start
+        start = time()
+        lu,piv = cpx.scipy.linalg.lu_factor(Ac)
+        lutestc = cp.any(cp.diag(cp.triu(lu))<1e-10)
+        lutimec = time() - start
+        print('\tPure Cupy(64?):', dettestc, dettimec, lutestc, lutimec)
             
 np.save('parityMatrices.npy', out)
